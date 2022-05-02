a1b6f20725edc1ed0758b59e87956296eba7705b
marlusmarcos <marlusufrn@gmail.com>
2022-05-02 09:02:14 -0300

    finalizando

--- activity01/ClientEvents/src/com/dimap/Main.java
+++ activity01/ClientEvents/src/com/dimap/Main.java
@@ -13,32 +13,24 @@ public static void main(String[] args) throws MalformedURLException, RemoteExcep
 		
 		ServerEvents server = (ServerEvents) 
 				Naming.lookup("rmi://127.0.0.1:1098/ServerEvents");
-	//	ServerEvents server2 = (ServerEvents) 
-		//		Naming.lookup("rmi://127.0.0.1:1098/ServerEvents");
+		Naming.lookup("rmi://127.0.0.1:1098/ServerEvents");
 
-		Observer client = new ObserverImpl("Jean");
+		Observer client1 = new ObserverImpl("Jean");
 		ObserverImpl client2 = new ObserverImpl("Marlus");
 		Observer client3 = new ObserverImpl("Mikael");
 		Observer client4 = new ObserverImpl("Marcos");
+		Observer client5 = new ObserverImpl("Andre");
 		
 		server.criarTopico("fuutebol");
 		server.getEvento(0).setObservers(client4);
 		server.getEvento(0).setObservers(client3);
 		server.getEvento(0).setObservers(client2);
 		server.criarTopico("filmes");
-		server.getEvento(1).setObservers(client);
-		server.getEvento(0).notificar("teste de notificacao do topico: " + server.getEvento(0).getNome() );
-		server.getEvento(1).notificar("teste de notificacao do topico: " + server.getEvento(1).getNome() );
+		server.getEvento(1).setObservers(client1);
+		server.getEvento(1).setObservers(client5);
+		server.getEvento(0).notificar("NOtificando para os interessados, ABC CAMPEÃO ESTADUAL 2022 com o placar de 4x2 em cima do 4meric4 no tópico: " + server.getEvento(0).getNome() );
+		server.getEvento(1).notificar("notificando todos sobre o filme do doutor estranho: " + server.getEvento(1).getNome() );
 		
-//		server.register(client2);
-//		server.register(client3);
-//		server.register(client4);
-//		server.notificarSubscriber("notificando sobre futebol ");
-//		server.notificarSubscriber("notificando sobre filme ");
-//		//server.notificarSubscriber("notificando sobre series ");
-//		//client2.getNotificacoes();
-//		for (String m : client2.getM_list()) {
-//			System.out.println(m);
-//		}
+
 	}
 }
---
+++ activity01/ClientEvents/src/com/dimap/ui/Terminal.java
@@ -0,0 +1,24 @@
+package com.dimap.ui;
+
+import java.rmi.Naming;
+
+import com.dimap.ServerEvents;
+
+public class Terminal {
+	
+
+	public void init () {
+		int key = 0;
+		
+		switch (key) {
+		case 0:
+			
+			
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+}
